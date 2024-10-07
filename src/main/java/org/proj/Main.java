276df5c03cac87610031408834ac60b0caf5759d
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-07 19:28:41 +0545

    Project Completed

--- src/main/java/org/proj/Main.java
+++ src/main/java/org/proj/Main.java
@@ -3,6 +3,7 @@ package org.proj;
 //This project is built in Jet Brains IDE
 //The Project is complete But is being worked on for more features
 
+
 public class Main {
     public static void main(String[] args) {
         WhackAMole whackamole = new WhackAMole();
